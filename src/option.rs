c94bb318df9495a195552fc2c306bf08b32a7f9a
Hisham <74142267+Hisham-inc@users.noreply.github.com>
2023-03-14 09:53:42 +0530

    Update option.rs

--- src/option.rs
+++ src/option.rs
@@ -1,6 +1,7 @@
 use scrypto::radix_engine_interface::time::*;
 use scrypto::prelude::*;
 
+/// Trying to do a cross-blueprint call but dont know how to do 
 external_blueprint! {
     ConstantSumAmm {
         fn instantiate_amm_pool(cc_token_a: Bucket, cc_token_b: Bucket, _strike_price: Decimal, bt_per_second: Bucket,
@@ -17,16 +18,19 @@ external_component! {
 #[blueprint]
 mod option_implementation {
     struct ConstantSumOption {
+        /// Vault where input token is locked 
         lock_token_vault: Vault,
         strike_price: Decimal,
         mint_badge_vault: Vault,
+        /// Resource address of bonded token
         bt_address: ResourceAddress,
+        /// Vault where collateral claim tokens are stored
         cct_vault: Vault,
+        /// Vault where bonded tokens are stored
         bt_vault: Vault,
+        /// Duration of the maturity of the option
         duration: i64
     }
-
-    /// strike rate should be in the amm function
 
     impl ConstantSumOption {
         pub fn instantiate_option(token_a_resource_address: ResourceAddress, token_b_resource_address: ResourceAddress,
@@ -60,7 +64,8 @@ mod option_implementation {
                 .burnable(rule!(require(mint_badge.resource_address())), LOCKED)
                 .divisibility(DIVISIBILITY_MAXIMUM)
                 .mint_initial_supply(strike_price * input_token.amount());
-
+            
+            /// Checking whether the token deposited is token_a or token_b
             if input_token.resource_address() == token_a_resource_address {cc_token = cctoken_a;}
             else {cc_token = cctoken_b;}
             
@@ -72,7 +77,7 @@ mod option_implementation {
                 .divisibility(DIVISIBILITY_MAXIMUM)
                 .mint_initial_supply(input_token.amount() / strike_price);
 
-             
+            /// Setting the maturity of the options contract 
             assert!(Clock::current_time_is_at_or_before(Instant::new(duration), TimePrecision::Minute) == true, "Maturity of the pool is over");
 
             let option_implementation: ConstantSumOptionComponent = Self {
@@ -93,6 +98,7 @@ mod option_implementation {
             self.lock_token_vault.amount();
         }
 
+        /// method for lending token 
         pub fn lend_tokens(&mut self, input_lend_token: Bucket, bt_interest: Decimal) -> (Bucket, Bucket) {
             if input_lend_token.resource_address() == self.lock_token_vault.resource_address() {
                 let bt_manager = borrow_resource_manager!(self.bt_address);
@@ -104,6 +110,7 @@ mod option_implementation {
             else {panic!("The provided token doesnt have an Amm pool")}
         }
 
+        /// method for borrowing tokens
         pub fn borrow_tokens(&mut self, input_collateral: Bucket, cct_interest: Decimal) -> (Bucket, Bucket) {
             if input_collateral.resource_address() == self.lock_token_vault.resource_address() {
                 let cct_manager = borrow_resource_manager!(self.cct_vault.resource_address());
